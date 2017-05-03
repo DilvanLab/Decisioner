ef2d917edc3aa5b66f05f5f64c671bee2b371f57
johnciclus <john@ciclus.co>
2017-05-03 10:59:25 -0300

    update blazegraph path

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -33,7 +33,7 @@ beans = {
     //TODO PegDown not thread safe: Take it out of this global variable!
     md(PegDownProcessor)
 
-    k(Know, 'http://143.107.232.73:9999/blazegraph/namespace/kb/sparql')
+    k(Know, 'http://127.0.0.1:9999/blazegraph/namespace/kb/sparql')
     //k(Know, 'http://192.168.0.103:9999/blazegraph/namespace/kb/sparql')
     //k(Know, 'http://192.168.0.14:9999/blazegraph/namespace/kb/sparql')
     //k(Know, 'http://192.168.4.28:9999/blazegraph/namespace/kb/sparql')
